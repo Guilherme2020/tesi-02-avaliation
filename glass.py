77be3fbfe74ed26de7201e8dd7b28f9a55f8241c
guilherme2020 <guilhermerodrigues123477@gmail.com>
2018-12-05 07:29:15 -0300

    import py

--- glass.py
+++ glass.py
@@ -3,5 +3,9 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('glass_data.csv’)
+df = pd.read_csv('glass_data.csv')
 
+df.columns = [ "id","indice_refrativo","sodio",'Magnesio',"Aluminio","Silicone","Potasion","Calcio","Barium","Ferro","n_columns"]
+
+
+print(df.head())
