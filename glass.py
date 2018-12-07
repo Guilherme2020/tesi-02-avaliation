640dd205142d70a54277124243b9831360c298a5
guilherme2020 <guilhermerodrigues123477@gmail.com>
2018-12-07 19:17:55 -0300

    dados

--- glass.py
+++ glass.py
@@ -8,13 +8,24 @@ df = pd.read_csv('glass_data.csv')
 df.columns = [ "id","indice_refrativo","sodio",'Magnesio',"Aluminio","Silicone","Potasion","Calcio","Barium","Ferro","n_columns"]
 
 
-nulos = df.isna().sum()
-
-tipos_colunas = df.dtypes
+df['n_columns'] = df['n_columns'].apply(lambda x : (0,1)[x <= 4])
 
 
 
-# print(df.head(10))
+# nulos = df.isna().sum()
 
-print("Campos nulos: ",nulos)
-# print("Tipos colunas: ",tipos_colunas)
+# tipos_colunas = df.dtypes
+
+media_sodio = df.sodio.mean()
+max_sodio = df.sodio.max()
+describe = df.describe()
+
+print(df.head(100))
+# #print(df.shape)
+# print("Campos nulos: ",nulos)
+
+# # print("Dados estatisticos das colunas: ")
+# print(describe)
+# print(media_sodio)
+# print(max_sodio)
+# #print("Tipos colunas: ",tipos_colunas)
