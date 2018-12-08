523a9c60abf0c6990bcee34f54388faccf042109
guilherme2020 <guilhermerodrigues123477@gmail.com>
2018-12-07 21:11:54 -0300

    notbo

--- glass.py
+++ glass.py
@@ -2,19 +2,19 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.ensemble import RandomForestClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.ensemble import RandomForestClassifier
 
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
+# from sklearn.ensemble import AdaBoostClassifier
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.tree import DecisionTreeClassifier
 
 
-from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import confusion_matrix
 
 
 
@@ -23,7 +23,7 @@ df = pd.read_csv('glass_data.csv')
 df.columns = [ "id","indice_refrativo","sodio",'Magnesio',"Aluminio","Silicone","Potasion","Calcio","Barium","Ferro","tipo"]
 
 
-df['n_columns'] = df['n_columns'].apply(lambda x : (0,1)[x <= 4])
+df['tipo'] = df['tipo'].apply(lambda x : (0,1)[x <= 4])
 
 
 
@@ -35,8 +35,8 @@ media_sodio = df.sodio.mean()
 max_sodio = df.sodio.max()
 describe = df.describe()
 
-print(df.head(100))
-# #print(df.shape)
+#print(df.head(100))
+print(df.shape)
 # print("Campos nulos: ",nulos)
 
 # # print("Dados estatisticos das colunas: ")
